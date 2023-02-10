203d2aa12962d31cba60abc460a6b7ab392d94b6
rebecca wood <rebeccaseverowood@gmail.com>
2023-02-11 11:55:28 +1300

    local storage differentiates between random and desired cocktail and saves desired into LS when savebtn clicked

--- assets/recipes/recipe.js
+++ assets/recipes/recipe.js
@@ -1,75 +1,87 @@
 // variable for all saved cocktail recipes
 const savedRecipes = $('#savedRecipes');
 
-// variable for cocktail name location
-const cocktailNameR = $('#Cocktail-Name');
-
-// variable for cocktail image location
-const cocktailImgR = $('#cocktailImg');
-
-// variable for cocktail ingredients location
-const cocktailIngR = $('#ingredients-Results');
-
-// variable for cocktail instruction location
-const cocktailInsR = $('#instructions-Results');
-
-// variable for pulling user saved cocktail from LS
-const savedCocktail = JSON.parse(localStorage.getItem('name'));
-
-localStorage['savedCocktail']=localStorage['name']; delete localStorage['name'];
-
-// variable to pull cocktail API w user saved cocktail
-const cocktailNameURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + savedCocktail;
-
-console.log(cocktailNameURL);
-
-$.ajax({
-    url: cocktailNameURL,
-    method: 'GET'
-}).then(function (response) {
-    console.log(response);
-
-
-});
 
 
 
+// // variable for cocktail name location
+// const cocktailNameR = $('#Cocktail-Name');
+
+// // variable for cocktail image location
+// const cocktailImgR = $('#cocktailImg');
+
+// // variable for cocktail ingredients location
+// const cocktailIngR = $('#ingredients-Results');
+
+// // variable for cocktail instruction location
+// const cocktailInsR = $('#instructions-Results');
+
+
+const savedCocktail = JSON.parse(localStorage.getItem('savedCocktail'));
 
 
 
+// creates a function 
+function createSavedCocktailButton(savedCocktail) {
+    //creates a variable that creates that makes a button with a saved cocktail name inside
+    let cocktailListItem = $("<button>").text(savedCocktail);
 
 
 
-//function to retrieve the cocktail info from LS and append to cocktail locations
-function getRecipe (){
+    //c
+    cocktailListItem.on('click', function (event) {
+        event.preventDefault();
+
+        const savedCocktail = JSON.parse(localStorage.getItem('savedCocktail'));
+
+        // variable to pull cocktail API w user saved cocktail
+        const cocktailNameURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + savedCocktail;
+        console.log(savedCocktail);
+        console.log(cocktailNameURL);
+
+        //function to run the API query
+        $.ajax({
+            url: cocktailNameURL,
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response);
+        });
+    })
+
+    savedRecipes.append(cocktailListItem);
     
-    //retrieving cocktail name from LS and adding to html
-    cocktailNameR.text(JSON.parse(localStorage.getItem('name')));
 
-    //retrieving cocktail ingredients from LS and adding to html
-    cocktailIngR.text(JSON.parse(localStorage.getItem('measure'), 'of', JSON.parse(localStorage.getItem('ingredients'))))
-
-    cocktailIngR.text(JSON.parse(localStorage.getItem('measure'), 'of', JSON.parse(localStorage.getItem('ingredients'))));
+}
 
 
-    //retrieving cocktail instructions from LS and adding to html
-    cocktailInsR.text(JSON.parse(localStorage.getItem('instructions')));
 
-    let imageLS = JSON.parse(localStorage.getItem('image'));
-    cocktailImgR.attr("src", imageLS);
+//TO DO
+// - Put saved cocktail into array
 
-};
 
-getRecipe();
+
+createSavedCocktailButton();
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Creates variable for the button ID
 const backBtn = $('#backBtn')
 // Initialises Jquery, adds a click function, takes the user to the home page and triggers the matchBtn event
 
-$(document).ready(function() {
-  backBtn.on('click', function() {
-    window.location.replace('../../index.html')
-    }) 
+$(document).ready(function () {
+    backBtn.on('click', function () {
+        window.location.replace('../../index.html')
+    })
     $('#matchBtn').click()
     // Appends it to the HTML
     $('#savedRecipes').append(backBtn)
