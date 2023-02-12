3a1c4f6e12e6539d5761a34333c469166502c948
Zwirled <bramwellamelia@gmail.com>
2023-02-12 12:13:21 +0000

    Recipe page styling, reordered HTML, amended JS to fit

--- assets/recipes/recipe.js
+++ assets/recipes/recipe.js
@@ -1,8 +1,10 @@
 // variable for all saved cocktail recipes
-const savedRecipes = $('#savedRecipes');
+const recipeList = $('#recipe-list');
 
 // // variable for cocktail name location
 const cocktailNameR = $('#Cocktail-Name');
+
+const singleCocktail = $('#single-cocktail');
 
 // variable for cocktail image location
 const cocktailImgR = $('#cocktailImg');
@@ -19,54 +21,54 @@ console.log(savedCocktailArr);
 
 
 
-function dynamicSavedCocktailButton(i){
+function dynamicSavedCocktailButton(i) {
 
-    for (let x = 0; x < savedCocktailArr.length; x++){
-        let cocktailListItem = $("<button>").text(i).attr("id", "savedCocktail" + x).attr("class", "savedCocktailButtons");
-        savedRecipes.append(cocktailListItem)
+    for (let x = 0; x < savedCocktailArr.length; x++) {
+        let cocktailListItem = $("<a>").text(i).attr("id", "savedCocktail" + x).attr("class", "saved-cocktail-buttons");
+        recipeList.append(cocktailListItem)
 
         cocktailListItem.on('click', function (event) {
             event.preventDefault();
             //replaces any spaces with % so able to search in the API
-            let savedCocktail = i.replace( / +/g, '%');
+            let savedCocktail = i.replace(/ +/g, '%');
             // replaceAll(' ','');
             console.log(savedCocktail);
             const cocktailNameURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + savedCocktail;
             console.log(cocktailNameURL);
-            
+
             $.ajax({
                 url: cocktailNameURL,
                 method: "GET"
-        
+
             }).then(function (response) {
 
-        let cocktailNameSrc = response.drinks[0].strDrink;
-        cocktailNameR.text(cocktailNameSrc);
+                let cocktailNameSrc = response.drinks[0].strDrink;
+                cocktailNameR.text(cocktailNameSrc);
 
-        let cocktailImgSrc = response.drinks[0].strDrinkThumb;
-        cocktailImgR.attr('src', cocktailImgSrc);
+                let cocktailImgSrc = response.drinks[0].strDrinkThumb;
+                cocktailImgR.attr('src', cocktailImgSrc);
 
-        for (let z = 1; z < 15; z++) {
-            let ingredient = response.drinks[0]["strIngredient" + z]
-            let measure = response.drinks[0]["strMeasure" + z]
-            // If the value is not null, add the them to an array of objects
-            if (ingredient !== null && measure !== null) {
-                let ingredients = []
-                ingredients.push({
-                    ingredient: ingredient,
-                    measure: measure
-                })
-                // Loops the ingredients array
-                for (let y = 0; y < ingredients.length; y++) {
-                    // Pulls data from the array, creates a Div and adds it to the HTML
-                    var ingredientx = ingredients[y].ingredient
-                    var measurex = ingredients[y].measure
-                    cocktailIngR.text(measurex + " of " + ingredientx)
+                for (let z = 1; z < 15; z++) {
+                    let ingredient = response.drinks[0]["strIngredient" + z]
+                    let measure = response.drinks[0]["strMeasure" + z]
+                    // If the value is not null, add the them to an array of objects
+                    if (ingredient !== null && measure !== null) {
+                        let ingredients = []
+                        ingredients.push({
+                            ingredient: ingredient,
+                            measure: measure
+                        })
+                        // Loops the ingredients array
+                        for (let y = 0; y < ingredients.length; y++) {
+                            // Pulls data from the array, creates a Div and adds it to the HTML
+                            var ingredientx = ingredients[y].ingredient
+                            var measurex = ingredients[y].measure
+                            cocktailIngR.text(measurex + " of " + ingredientx)
+                        }
+                    }
                 }
-            }
-        }
-        const cocktailInstrSrc = response.drinks[0].strInstructions;
-        cocktailInsR.text(cocktailInstrSrc)
+                const cocktailInstrSrc = response.drinks[0].strInstructions;
+                cocktailInsR.text(cocktailInstrSrc)
             });
         });
     }
@@ -90,6 +92,6 @@ $(document).ready(function () {
     })
     $('#matchBtn').click()
     // Appends it to the HTML
-    $('#savedRecipes').append(backBtn)
+    $('#recipe-list').append(backBtn);
 })
 
